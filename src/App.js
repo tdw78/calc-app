ffc4c250f9ef24ab0f8da37b5288f179811844d4
Tim Weiss <tdweiss1978@gmail.com>
2020-08-04 23:54:13 -0700

    Math Done

--- src/App.js
+++ src/App.js
@@ -39,6 +39,57 @@ clearDisplay = () => {
   this.setState({ display: "" })
 }
 
+add = () => {
+  this.state.prevNum = this.state.display
+  this.setState({ display: "" })
+  this.state.operator = "plus"
+}
+
+subtract = () => {
+  this.state.prevNum = this.state.display
+  this.setState({ display: "" })
+  this.state.operator = "subtract"
+}
+
+divide = () => {
+  this.state.prevNum = this.state.display
+  this.setState({ display: "" })
+  this.state.operator = "divide"
+}
+
+multiply = () => {
+  this.state.prevNum = this.state.display
+  this.setState({ display: "" })
+  this.state.operator = "multiply"
+}
+
+evaluate = () => {
+  this.state.currNum = this.state.display
+  if(this.state.operator === "plus"){
+    this.setState({ 
+      display: parseFloat(this.state.prevNum) + 
+               parseFloat(this.state.currNum) })
+  }
+
+  if(this.state.operator === "subtract"){
+    this.setState({ 
+      display: parseFloat(this.state.prevNum) - 
+               parseFloat(this.state.currNum) })
+  }
+
+  if(this.state.operator === "divide"){
+    this.setState({ 
+      display: parseFloat(this.state.prevNum) / 
+               parseFloat(this.state.currNum) })
+  }
+
+  if(this.state.operator === "multiply"){
+    this.setState({ 
+      display: parseFloat(this.state.prevNum) * 
+               parseFloat(this.state.currNum) })
+  }
+
+}
 
   render() {
 
@@ -54,35 +105,35 @@ clearDisplay = () => {
           <Clear handleClear={this.clearDisplay}>AC</Clear>
           <Function>+/-</Function>
           <Function>%</Function>
-          <Operator>/</Operator>
+          <Operator handleClick={this.divide}>/</Operator>
         </div>
         
         <div className="button-row">
           <Button handleClick={this.addToDisplay}>7</Button>
           <Button handleClick={this.addToDisplay}>8</Button>
           <Button handleClick={this.addToDisplay}>9</Button>
-          <Operator>X</Operator>
+          <Operator handleClick={this.multiply}>X</Operator>
         </div>
 
         <div className="button-row">
           <Button handleClick={this.addToDisplay}>4</Button>
           <Button handleClick={this.addToDisplay}>5</Button>
           <Button handleClick={this.addToDisplay}>6</Button>
-          <Operator>-</Operator>
+          <Operator handleClick={this.subtract}>-</Operator>
         </div>
 
         <div className="button-row">
           <Button handleClick={this.addToDisplay}>1</Button>
           <Button handleClick={this.addToDisplay}>2</Button>
           <Button handleClick={this.addToDisplay}>3</Button>
-          <Operator>+</Operator>
+          <Operator handleClick={this.add}>+</Operator>
         </div>
 
         <div className="button-row">
           <Button></Button>
           <Button handleClick={this.addZeroToDisplay}>0</Button>     
           <Button handleClick={this.addDecimalToDisplay}>.</Button>
-          <Operator>=</Operator>
+          <Operator handleClick={this.evaluate}>=</Operator>
         </div>
 
       </div>
--- src/Components/Operator.js
+++ src/Components/Operator.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Operator extends Component {
   render() {
   return (
-    <div className="operator">
+    <div className="operator"
+      onClick={() => this.props.handleClick(this.props.children)}
+    >
       {this.props.children}
     </div>
   );
